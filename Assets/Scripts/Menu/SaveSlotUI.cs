7f5ada249eb3b6c82b767694e30d3a0ab5a340a5
劣仁假 <a729121423@gmail.com>
2025-04-21 23:35:15 +0800

    95 主菜单UI

---
+++ Assets/Scripts/Menu/SaveSlotUI.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveSlotUI : MonoBehaviour
+{
+    public Text dataTime, dataScene;
+    private Button currentButton;
+
+    private int Index => transform.GetSiblingIndex();
+    
+    private void Awake()
+    {
+        currentButton = GetComponent<Button>();
+        currentButton.onClick.AddListener(LoadGameData);
+    }
+
+    private void LoadGameData()
+    {
+        
+    }
+}
